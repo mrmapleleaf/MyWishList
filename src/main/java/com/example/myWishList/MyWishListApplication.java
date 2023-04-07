400f064d763416cde3503baf3ecfaf349a8380ca
mrmapleleaf <kaple.de.96@gmail.com>
2023-04-08 00:36:41 +0900

    create project

---
+++ src/main/java/com/example/myWishList/MyWishListApplication.java
@@ -0,0 +1,13 @@
+package com.example.myWishList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWishListApplication.class, args);
+	}
+
+}
